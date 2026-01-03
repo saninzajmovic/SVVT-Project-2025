c6f81c093da28cb37dbd5f0d2ad44913abc972f4
Mirza <107308953+MirzaSijamic@users.noreply.github.com>
2026-01-03 22:13:15 +0100

    Price range test

---
+++ src/test/java/org/example/ProductFilteringSortingTest.java
@@ -0,0 +1,113 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductFilteringSortingTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp2() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C:/Users/MS/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void FilterByPriceRange(){
+        driver.get("https://www.ekupi.ba/bs/");
+        driver.manage().window().fullscreen();
+
+        WebElement toHover = driver.findElement(By.xpath("/html/body/main/header/nav[3]/div/ul[3]/li[3]/span[1]/a"));
+
+        Actions action = new Actions(driver);
+
+        action.moveToElement(toHover).perform();
+
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement RacKomp = driver.findElement(By.xpath("/html/body/main/header/nav[3]/div/ul[3]/li[3]/div/div/div[2]/ul/li[4]/a"));
+
+        RacKomp.click();
+
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.manage().window().fullscreen();
+
+        WebElement text = driver.findElement(By.xpath("/html/body/main/div[5]/div[1]/h1"));
+
+        assertEquals("Računarske komponente", text.getText());
+
+        WebElement lowPrice = driver.findElement(By.xpath("//*[@id=\"product-facet\"]/div[3]/div[2]/ul/li[2]/div/div[1]/input"));
+        lowPrice.clear();
+        lowPrice.sendKeys("67");
+
+        WebElement highPrice = driver.findElement(By.xpath("//*[@id=\"product-facet\"]/div[3]/div[2]/ul/li[2]/div/div[2]/input"));
+        highPrice.clear();
+        highPrice.sendKeys("99");
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"product-facet\"]/div[3]/div[2]/div/span/a"));
+        submit.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.manage().window().fullscreen();
+
+        WebElement finalText = driver.findElement(By.xpath("//*[@id=\"product-facet\"]/div[1]/div[2]/ul/li/form"));
+        String actualText = finalText.getText().trim();
+        assertEquals("Cijena Od 67 KM Do 99 KM", actualText);
+    }
+}
+/*
+/html/body/main/div[5]/div[2]/div[2]/div/div/div[2]/div[17]/div[1]/div/div[2]/div[2]
+
+/html/body/main/div[5]/div[2]/div[2]/div/div/div[2]/div[5]/div[1]/div/div[2]/div[2]
+/html/body/main/div[5]/div[2]/div[2]/div/div/div[2]/div[4]/div[1]/div/div[2]/div[2]
+/html/body/main/div[5]/div[2]/div[2]/div/div/div[2]/div[3]/div[1]/div/div[2]/div[2]
+/html/body/main/div[5]/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[2]
+/html/body/main/div[5]/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[2]
+*/

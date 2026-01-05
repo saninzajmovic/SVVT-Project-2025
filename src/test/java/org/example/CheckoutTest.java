9f1b536cb3f5243fa3f17d27385ed79192ef1b88
Mirza <107308953+MirzaSijamic@users.noreply.github.com>
2026-01-05 03:52:39 +0100

    test cases done

--- src/test/java/org/example/CheckoutTest.java
+++ src/test/java/org/example/CheckoutTest.java
@@ -12,8 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CheckoutTest {
 
@@ -28,8 +27,22 @@ public class CheckoutTest {
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+
         driver.manage().window().maximize();
 
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void checkoutGuest(){
         driver.get("https://www.ekupi.ba/bs/bf-2025-dodatni-benefit/ekupi-novogodisnji-slatki-paketic-savrsen-poklon-za-djecu/p/EK000786795");
         //driver.manage().window().fullscreen();
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -45,6 +58,12 @@ public class CheckoutTest {
 
         WebElement cookie = driver.findElement(By.xpath("//*[@id=\"js-cookie-notification\"]/div/button[2]"));
         cookie.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         WebElement popup = driver.findElement(By.xpath("//*[@id=\"cboxLoadedContent\"]/div/div[3]/a[2]"));
         popup.click();
@@ -66,17 +85,6 @@ public class CheckoutTest {
         }catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    @Test
-    public void checkoutGuest(){
 
         //*[@id="guest.email"]
         WebElement email = driver.findElement(By.xpath("//*[@id=\"guest.email\"]\n"));
@@ -103,6 +111,112 @@ public class CheckoutTest {
         assertEquals("29,90 KM", price.getText(),"not equal");
 
         //*[@id="sideTotal"]/div
+    }
+
+    @Test
+    public void testCheckoutWithEmptyCart() {
+        driver.get("https://www.ekupi.ba/bs/cart");
+
+        WebElement emptyMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[contains(text(), 'Vaša košarica je prazna') or contains(@class, 'cart-empty')]")
+        ));
+        assertTrue(emptyMessage.isDisplayed(), "User should see 'Cart is empty' message");
+
+        boolean isCheckoutButtonPresent = driver.findElements(By.cssSelector(".btn--continue-checkout")).size() > 0;
+
+        if (isCheckoutButtonPresent) {
+            WebElement btn = driver.findElement(By.cssSelector(".btn--continue-checkout"));
+            String classes = btn.getAttribute("class");
+            assertTrue(classes.contains("disabled") || !btn.isEnabled(),
+                    "CRITICAL BUG: Checkout button is enabled even though cart is empty!");
+        } else {
+            System.out.println("Pass: Checkout button is correctly hidden.");
+        }
+
+        driver.get("https://www.ekupi.ba/bs/checkout/multi/login");
+
+        String currentUrl = driver.getCurrentUrl();
+        assertFalse(currentUrl.contains("/checkout/multi/"),
+                "Security Fail: Site allowed access to Checkout page with an empty cart!");
+    }
+
+    @Test
+    public void testAddressFormValidation() {
+        // 1. Prepare State
+        driver.get("https://www.ekupi.ba/bs/bf-2025-dodatni-benefit/ekupi-novogodisnji-slatki-paketic-savrsen-poklon-za-djecu/p/EK000786795");
+        //driver.manage().window().fullscreen();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        WebElement cartButton = driver.findElement(By.xpath(" //*[@id=\"addToCartButton\"]"));
+        cartButton.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement cookie = driver.findElement(By.xpath("//*[@id=\"js-cookie-notification\"]/div/button[2]"));
+        cookie.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement popup = driver.findElement(By.xpath("//*[@id=\"cboxLoadedContent\"]/div/div[3]/a[2]"));
+        popup.click();
+
+        WebElement cartButton2 = driver.findElement(By.xpath("/html/body/main/header/nav[2]/div/div[2]/div[2]/ul[1]/li/div/div/div[1]"));
+        cartButton2.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement cartButton3 = driver.findElement(By.xpath("/html/body/main/div[5]/div[3]/div[5]/div/div/div[1]/button"));
+        cartButton3.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //*[@id="guest.email"]
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"guest.email\"]\n"));
+        email.sendKeys("dummy@gmail.com");
+
+        //*[@id="guest.confirm.email"]
+        WebElement email2 = driver.findElement(By.xpath("//*[@id=\"guest.confirm.email\"]"));
+        email2.sendKeys("dummy@gmail.com");
+
+        WebElement guest = driver.findElement(By.xpath("//*[@id=\"guestForm\"]/div[3]/button"));
+        guest.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 2. We are now on Address Page. Try to click "Next" (Dalje) immediately
+        WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("addressSubmit")));
+        nextButton.click();
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement negativeText = driver.findElement(By.xpath("/html/body/main/div[5]/div[1]/div"));
+        assertEquals("×\n" +
+                        "Pronađena je greška za adresu koju ste unijeli. Molimo Vas da provjerite greške i ponovno unesete Vašu adresu.",
+                negativeText.getText(), "Greška");
 
     }
 
---
+++ src/test/java/org/example/InputValidationErrorHandlingTest.java
@@ -0,0 +1,141 @@
+package org.example;
+
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InputValidationErrorHandlingTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/MS/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 1. Hide the "AutomationControlled" flag
+        options.addArguments("--disable-blink-features=AutomationControlled");
+
+        // 2. Remove the "Chrome is being controlled by automated software" bar
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+
+        // 3. Add a User-Agent (Pretend to be a real human browser)
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        driver.get("https://www.ekupi.ba/bs/login");
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void invalidEmailTest(){
+        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"register.email\"]")));
+        email.sendKeys("bad_email_format");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        WebElement errorMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#register\\.email-error, .parsley-error, .help-block")
+        ));
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0, 1500)");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.findElement(By.xpath("//*[@id=\"registerChkTermsConditions\"]\n")).click();
+
+        //*[@id="register-submit-btn"]
+        driver.findElement(By.xpath("//*[@id=\"register-submit-btn\"]")).click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        jse.executeScript("window.scrollTop");
+
+        assertTrue(errorMsg.getText().equals("Unesite ispravan email") || errorMsg.isDisplayed());
+    }
+
+    @Test
+    public void shortPasswordTest(){
+        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]")));
+        email.sendKeys("kratak");
+
+
+        WebElement errorMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#register\\.email-error, .parsley-error, .help-block")
+        ));
+
+        //*[@id="registerChkTermsConditions"]
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0, 1500)");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.findElement(By.xpath("//*[@id=\"registerChkTermsConditions\"]\n")).click();
+
+        //*[@id="register-submit-btn"]
+        driver.findElement(By.xpath("//*[@id=\"register-submit-btn\"]")).click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        jse.executeScript("window.scrollTop");
+
+        assertTrue(errorMsg.getText().equals("Lozinka mora imati najmanje 6 znakova") || errorMsg.isDisplayed());
+    }
+
+
+}
